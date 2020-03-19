159166411374f334fe704b34f8ee8cfb323190cc
LuosRestil <metaltasteslikechicken@gmail.com>
2020-03-19 16:54:14 -0500

    Fixed typo in User model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
-  username: { type: String, unique: true, require: true, dropDups: true },
+  username: { type: String, unique: true, required: true, dropDups: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   // roles are admin, support, or user
